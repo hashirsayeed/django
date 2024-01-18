a8822d81203c35d75702b20e0cd8762f1329fe19
hashirsayeed <hashirsayeed@gmail.com>
2024-01-18 15:18:43 +0000

    Commit on 18 Jan

--- django_proj/blog/views.py
+++ django_proj/blog/views.py
@@ -30,4 +30,4 @@ def home(request):
 
 def about(request):
     # return HttpResponse('<h1>Blog About</h1>')
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})

be66ca20eec36b9467f47e4146334b983268d8f6
hashirsayeed <hashirsayeed@gmail.com>
2024-01-23 10:36:26 +0000

    commit on 23 jan

--- django_proj/blog/views.py
+++ django_proj/blog/views.py
@@ -1,29 +1,31 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
 
 
 #sending dummy data using post
 
-posts = [
-    {
-        'author': 'Hash',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 17, 2024'
-    },
-    {
-        'author': 'Patrick',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'January 16, 2024'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Hash',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'January 17, 2024'
+#     },
+#     {
+#         'author': 'Patrick',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'January 16, 2024'
+#     }
+# ]
 
 # Create your views here.
 
 def home(request):
     context = {
-        'posts': posts
+        # 'posts': posts
+        'posts' : Post.objects.all()
     }
     # return HttpResponse('<h1>Blog Home</h1>')
     return render(request, 'blog/home.html', context)

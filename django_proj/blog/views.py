57102ac17acf388a502d97b80ad1250cc9d1f488
hashirsayeed <hashirsayeed@gmail.com>
2024-02-04 19:26:04 +0000

    4 Feb

--- django_proj/blog/views.py
+++ django_proj/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 # from django.http import HttpResponse
 
 
@@ -33,3 +34,12 @@ def home(request):
 def about(request):
     # return HttpResponse('<h1>Blog About</h1>')
     return render(request, 'blog/about.html', {'title': 'About'})
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post

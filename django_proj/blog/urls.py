2b0fd818042da7c64ba626c3948e0ac5ccaff57a
hashirsayeed <hashirsayeed@gmail.com>
2024-02-06 10:07:13 +0000

    6 Feb

--- django_proj/blog/urls.py
+++ django_proj/blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     # path('', views.home, name = 'blog_home'),
     path('', PostListView.as_view(), name = 'blog_home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name = 'post_detail'),
+    path('post/new/', PostCreateView.as_view(), name = 'post_create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name = 'post_update'),
     path('about/', views.about, name = 'blog_about'),
 ]

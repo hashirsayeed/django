ad1d1ce395cc2325c60fa28d3c28e857453c2c6e
hashirsayeed <hashirsayeed@gmail.com>
2024-02-13 10:56:43 +0000

    13 Feb

--- django_proj/django_proj/settings.py
+++ django_proj/django_proj/settings.py
@@ -133,3 +133,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog_home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAI_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
--- django_proj/django_proj/urls.py
+++ django_proj/django_proj/urls.py
@@ -27,6 +27,9 @@ urlpatterns = [
     path('profile/', users_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', users_views.logout_view, name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password_reset/done', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('', include('blog.urls')),
 ]
 
